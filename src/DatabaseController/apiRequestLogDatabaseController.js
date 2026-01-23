4619ab74d59306a91f0c2ccf0b4834280da83792
aamirhannan <aamirhannan08@gmail.com>
2026-01-24 03:19:42 +0530

    api fixed

--- src/DatabaseController/apiRequestLogDatabaseController.js
+++ src/DatabaseController/apiRequestLogDatabaseController.js
@@ -2,7 +2,8 @@
 export const fetchApiRequestLogs = async (client) => {
     const { data, error } = await client
         .from('api_request_logs')
-        .select('*');
+        .select('*')
+        .order('created_at', { ascending: false });
 
     if (error) throw error;
     return data;
--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -1,10 +1,12 @@
 const camelToSnake = (obj) => {
+    if (!obj) return null;
     return Object.fromEntries(
         Object.entries(obj).map(([key, value]) => [key.replace(/([A-Z])/g, "_$1").toLowerCase(), value])
     );
 };
 
 const snakeToCamel = (obj) => {
+    if (!obj) return null;
     return Object.fromEntries(
         Object.entries(obj).map(([key, value]) => [key.replace(/(_[a-z])/g, (match) => match[1].toUpperCase()), value])
     );
